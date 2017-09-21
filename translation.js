907345f40924a673ac3a7ddb0b456b9f910c9606
GracieChristine <graciechristineli@gmail.com>
2017-09-21 00:28:28 -0600

    Styling Translation section.

--- translation.js
+++ translation.js
@@ -13,22 +13,3 @@ function showIt() {
     return errorThrown;
   });
 }
-
-// function showIt()
-//     {
-//         $.get("https://www.googleapis.com/language/translate/v2",
-//             {
-//             key:"AIzaSyBPNKaYskcfvuUWyDRQ6n9ZzTGFbLfMaHY",
-//             source:"en",
-//             target:$("#target").val(),
-//             q:$("#text").val()
-//             },
-//             function(response)
-//             {
-//                 $("#translated").html(response.data.translations[0].translatedText);
-//
-//             },"json") .fail(function(jqXHR, textStatus, errorThrown)
-//             {
-//                 alert( "error :"+errorThrown );
-//             });
-//     }
