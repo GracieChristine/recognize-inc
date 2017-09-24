6345478fe06c88cede3799da04598ba3b8ac6bf5
GracieChristine <graciechristineli@gmail.com>
2017-09-24 12:27:20 -0600

    Installing dependencies and fixing package bug.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react';
-import {  } from 'react-bootstrap';
+import React, {Component} from 'react';
+import {Navbar, Nav} from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">Recognize Inc.</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+        </Nav>
+      </Navbar>
     );
   }
 }
