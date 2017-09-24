8d70505f31d3bb09c353226178f3b8a828cca0f3
GracieChristine <graciechristineli@gmail.com>
2017-09-24 13:26:01 -0600

    Adding a sticky footer.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,39 @@
 import React, {Component} from 'react';
-import {Navbar, Nav, NavItem} from 'react-bootstrap';
+import {Navbar} from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <div>
+        {/* NAVBAR */}
       <Navbar>
         <Navbar.Header>
           <Navbar.Brand>
-            <a href="#">React-Bootstrap</a>
+            <a href="#">Recognize Inc.</a>
           </Navbar.Brand>
         </Navbar.Header>
-        <Nav>
-          <NavItem eventKey={1} href="#">Link</NavItem>
-          <NavItem eventKey={2} href="#">Link</NavItem>
-        </Nav>
+        <Navbar.Text pullRight>
+          A simple speech translational plateform
+        </Navbar.Text>
       </Navbar>
+      {/* CONTENT */}
+      <div className="wrapper">
+
+
+      </div>
+      {/* FOOTER */}
+      <div className="footer">
+        <p>
+          &copy; 2017
+          <script type="text/javascript">
+            new Date().getFullYear()>2017&&document.write("-"+new Date().getFullYear());
+          </script>
+          , Recognize Inc.
+        </p>
+      </div>
+    </div>
+
     );
   }
 }
