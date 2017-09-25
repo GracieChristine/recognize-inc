99d5bf13670144dcd2397f88857b4225cbbe8ba2
GracieChristine <graciechristineli@gmail.com>
2017-09-25 16:19:48 -0600

    Trying out translation component.

--- src/components/TextTranslation.jsx
+++ src/components/TextTranslation.jsx
@@ -1,9 +1,33 @@
 import React, {PropTypes, Component} from 'react';
 import {Grid, Row, Col} from 'react-bootstrap';
 
+import Config from './../config';
 import './TextTranslation.css';
 
 class TextTranslation extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      textInput: '',
+      textOutpu: '',
+      langSource: 'en',
+      langTarget: 'en'
+    };
+  }
+
+  translating(textInput, langSource, langTarget) {
+    fetch(`https://translation.googleapis.com/language/translate/v2?q=${textInput}&target=${langTarget}&format=text&source=${langSource}&key=${Config.googleAPIKey}`)
+      .then((response) => response.json())
+      .tehn((responseJSON) => {
+        console.log(responseJSON.data.translations[0].translatedText);
+        const text = responseJSON.data.translations[0].translatedText;
+        this.setState({textOutput: text})
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
   render() {
 
