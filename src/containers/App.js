22b9279ab4713fb0f0dc6c88de601bbffa314bde
GracieChristine <graciechristineli@gmail.com>
2017-09-26 14:04:04 -0600

    Fixing the footer.

--- src/containers/App.js
+++ src/containers/App.js
@@ -39,6 +39,8 @@ class App extends Component {
         <PageBreakerThree />
         <TextTranslation />
 
+        <div className="push"></div>
+
       </div>
       {/* FOOTER */}
       <div className="footer">
