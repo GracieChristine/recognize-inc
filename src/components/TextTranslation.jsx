d1f1feb2386f7baee78f0c83793ccf351970a0dd
GracieChristine <graciechristineli@gmail.com>
2017-09-25 21:17:29 -0600

    Removing codes for my first attemps on translation.

--- src/components/TextTranslation.jsx
+++ src/components/TextTranslation.jsx
@@ -6,28 +6,7 @@ import './TextTranslation.css';
 
 class TextTranslation extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      textInput: '',
-      textOutpu: '',
-      langSource: 'en',
-      langTarget: 'en'
-    };
-  }
 
-  translating(textInput, langSource, langTarget) {
-    fetch(`https://translation.googleapis.com/language/translate/v2?q=${textInput}&target=${langTarget}&format=text&source=${langSource}&key=${Config.googleAPIKey}`)
-      .then((response) => response.json())
-      .tehn((responseJSON) => {
-        console.log(responseJSON.data.translations[0].translatedText);
-        const text = responseJSON.data.translations[0].translatedText;
-        this.setState({textOutput: text})
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
 
   render() {
 
@@ -36,7 +15,7 @@ class TextTranslation extends Component {
       <Grid>
         <Row>
           <Col>
-            <h1>I'm working on the Translation section.</h1>
+            
           </Col>
         </Row>
       </Grid>
